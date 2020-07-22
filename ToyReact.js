e9c144442464e5c0b919c622794884ee181b93b3
ModeYapu <1207565967@qq.com>
2020-07-22 22:29:16 +0800

    vdom

--- ToyReact.js
+++ ToyReact.js
@@ -1,42 +1,98 @@
 "use strict";
+
+let childrenSymbol = Symbol("children");
 class ElementWrapper {
   constructor(type) {
-    this.root = document.createElement(type);
+    this.type = type;
+    this.props = Object.create(null);
+    this.children = [];
+    this[childrenSymbol] = [];
   }
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
-      this.root.addEventListener(eventName, value);
-    }
-    if (name === "className") {
-      name = "class";
-    }
-    this.root.setAttribute(name, value);
+    // if (name.match(/^on([\s\S]+)$/)) {
+    //   let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+    //   this.root.addEventListener(eventName, value);
+    // } else {
+    //   if (name === "className") {
+    //     name = "class";
+    //   }
+    //   this.root.setAttribute(name, value);
+    // }
+    this.props[name] = value;
   }
+
+  // get children() {
+  //   return this[childrenSymbol].map((child) => child.vdom);
+  // }
+
   appendChild(vchild) {
-    let range = document.createRange();
-    if (this.root.children.length) {
-      range.setStartAfter(this.root.lastChild);
-      range.setEndAfter(this.root.lastChild);
-    } else {
-      range.setStart(this.root, 0);
-      range.setEnd(this.root, 0);
-    }
-    vchild.mountTo(range);
+    this[childrenSymbol].push(vchild);
+    this.children.push(vchild.vdom);
+    // let range = document.createRange();
+    // if (this.root.children.length) {
+    //   range.setStartAfter(this.root.lastChild);
+    //   range.setEndAfter(this.root.lastChild);
+    // } else {
+    //   range.setStart(this.root, 0);
+    //   range.setEnd(this.root, 0);
+    // }
+    // vchild.mountTo(range);
   }
+
+  get vdom() {
+    return this;
+  }
+
   mountTo(range) {
+    this.range = range;
+    let placeholder = document.createComment("placeholder");
+    let endRange = document.createRange();
+    endRange.setStart(range.endContainer, range.endOffset);
+    endRange.setEnd(range.endContainer, range.endOffset);
+    endRange.insertNode(placeholder)
     range.deleteContents();
-    range.insertNode(this.root);
+    let element = document.createElement(this.type);
+    for (const name in this.props) {
+      let value = this.props[name];
+      element.setAttribute(name, value);
+      if (name.match(/^on([\s\S]+)$/)) {
+        let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+        element.addEventListener(eventName, value);
+      }
+      if (name === "className") {
+        element.setAttribute("class", value);
+      }
+      element.setAttribute(name, value);
+    }
+    for (const child of this.children) {
+      let range = document.createRange();
+      if (element.children.length) {
+        range.setStartAfter(element.lastChild);
+        range.setEndAfter(element.lastChild);
+      } else {
+        range.setStart(element, 0);
+        range.setEnd(element, 0);
+      }
+      child.mountTo(range);
+    }
+    range.insertNode(element);
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
+    this.type = "#text";
+    this.children = [];
+    this.props = Object.create(null);
   }
   mountTo(range) {
+    this.range = range;
     range.deleteContents();
     range.insertNode(this.root);
+  }
+  get vdom() {
+    return this;
   }
 }
 
@@ -45,9 +101,13 @@ export class Component {
     this.children = [];
     this.props = Object.create(null);
   }
+
+  get type() {
+    return this.constructor.name;
+  }
   setAttribute(name, value) {
     if (name.match(/^on([\s\S]+)$/)) {
-      console.log(RegExp.$1);
+      // console.log(RegExp.$1);
     }
     this.props[name] = value;
     this[name] = value;
@@ -59,27 +119,92 @@ export class Component {
   }
 
   update() {
-    let placeholder = document.createElement("placeholder");
-    let range = document.createRange();
-    range.setStart(this.range.endContainer, this.range.endOffset);
-    range.setEnd(this.range.endContainer, this.range.endOffset);
-    range.insertNode(placeholder);
-    this.range.deleteContents();
-    let vdom = this.render();
-    vdom.mountTo(this.range);
-    // placeholder.parentNode.removeChild(placeholder);
+    let vdom = this.vdom;
+    console.log(vdom);
+    if (this.oldVdom) {
+      let isSameNode = (node1, node2) => {
+        if (node1.type !== node2.type) {
+          return false;
+        }
+        for (const name in node1.props) {
+          // if (
+          //   typeof node1.props[name] === "function" &&
+          //   typeof node2.props[name] === "function" &&
+          //   node1.props[name].toString() === node2.props[name].toString()
+          // ) {
+          //   continue;
+          // }
+          if (
+            typeof node1.props[name] === "object" &&
+            typeof node2.props[name] === "object" &&
+            JSON.stringify(node1.props[name]) ===
+              JSON.stringify(node2.props[name])
+          ) {
+            continue;
+          }
+          if (node1.props[name] !== node2.props[name]) {
+            return false;
+          }
+        }
+        if (
+          Object.keys(node1.props).length !== Object.keys(node2.props).length
+        ) {
+          return false;
+        }
+        return true;
+      };
+
+      // 比对children
+      let isSameTree = (node1, node2) => {
+        if (!isSameNode(node1, node2)) {
+          return false;
+        }
+        if (node1.children.length !== node2.children.length) {
+          return false;
+        }
+        for (let i = 0; i < node1.children.length; i++) {
+          if (!isSameTree(node1.children[i], node2.children[i])) {
+            return false;
+          }
+        }
+        return true;
+      };
+      let replace = (newTree, oldTree) => {
+        if (isSameTree(newTree, oldTree)) {
+          return;
+        }
+        if (!isSameNode(newTree, oldTree)) {
+          newTree.mountTo(oldTree.range);
+        } else {
+          for (let i = 0; i < newTree.children.length; i++) {
+            replace(newTree.children[i], oldTree.children[i]);
+          }
+        }
+      };
+      replace(vdom, this.oldVdom);
+    } else {
+      vdom.mountTo(this.range);
+    }
+    this.oldVdom = vdom;
+  }
+
+  get vdom() {
+    return this.render().vdom;
   }
 
   appendChild(vchild) {
     this.children.push(vchild);
   }
-
   setState(state) {
     let merge = (oldState, newState) => {
-      for (let p in newState) {
-        if (typeof newState[p] === "object") {
+      for (const p in newState) {
+        if (typeof newState[p] === "object" && newState[p] != null) {
           if (typeof oldState[p] !== "object") {
-            oldState[p] = {};
+            if (newState[p] instanceof Array) {
+              oldState[p] = [];
+            } else {
+              oldState[p] = {};
+            }
           }
           merge(oldState[p], newState[p]);
         } else {
@@ -94,53 +219,58 @@ export class Component {
     this.update();
   }
 }
-const ToyReact = {
-  createElement(type, attributes, ...children) {
-    // console.log(arguments);
-    let element;
-    if (typeof type === "string") {
-      element = new ElementWrapper(type);
-    } else {
-      element = new type();
-    }
-    for (const name in attributes) {
-      element.setAttribute(name, attributes[name]);
-    }
-    let insertChildren = (children) => {
-      for (let child of children) {
-        if (typeof child === "object" && child instanceof Array) {
-          insertChildren(child);
-        } else {
-          if (
-            !(child instanceof Component) &&
-            !(child instanceof ElementWrapper) &&
-            !(child instanceof TextWrapper)
-          ) {
-            child = String(child);
-          }
-          if (typeof child === "string") {
-            child = new TextWrapper(child);
-          }
-          element.appendChild(child);
+
+const createElement = (type, attributes, ...children) => {
+  // console.log(arguments);
+  let element;
+  if (typeof type === "string") {
+    element = new ElementWrapper(type);
+  } else {
+    element = new type();
+  }
+  for (const name in attributes) {
+    element.setAttribute(name, attributes[name]);
+  }
+  let insertChildren = (children) => {
+    for (let child of children) {
+      if (typeof child === "object" && child instanceof Array) {
+        insertChildren(child);
+      } else {
+        if (child === null || child === void 0) {
+          child = "";
         }
+        if (
+          !(child instanceof Component) &&
+          !(child instanceof ElementWrapper) &&
+          !(child instanceof TextWrapper)
+        ) {
+          child = String(child);
+        }
+        if (typeof child === "string") {
+          child = new TextWrapper(child);
+        }
+        element.appendChild(child);
       }
-    };
-    insertChildren(children);
-    return element;
-  },
-  render(vdom, element) {
-    let range = document.createRange();
-    if (element.children.length) {
-      range.setStartAfter(element.lastChild);
-      range.setEndAfter(element.lastChild);
-    } else {
-      range.setStart(element, 0);
-      range.setEnd(element, 0);
     }
-    vdom.mountTo(range);
-  },
+  };
+  insertChildren(children);
+  return element;
 };
 
-ToyReact.Component = Component;
+const render = (vdom, element) => {
+  let range = document.createRange();
+  if (element.children.length) {
+    range.setStartAfter(element.lastChild);
+    range.setEndAfter(element.lastChild);
+  } else {
+    range.setStart(element, 0);
+    range.setEnd(element, 0);
+  }
+  vdom.mountTo(range);
+};
 
-export default ToyReact;
+export default {
+  Component,
+  createElement,
+  render,
+};
--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 "use strict";
 import ToyReact, { Component } from "./ToyReact";
 
+
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {" "}
-        {this.state.value ? this.state.value : ""}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
@@ -20,7 +14,12 @@ class Square extends Component {
 
 class Board extends Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
@@ -55,12 +54,13 @@ class Game extends Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -73,26 +73,79 @@ class Game extends Component {
           squares: squares,
         },
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
-}
 
-class MyComponent extends Component {
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
-      <div>
-        <span>hello </span>
-        <span>word</span>
-        <span>!</span>
-        <div>
-          {true}
-          {this.children}
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
-const a = <Board></Board>;
-ToyReact.render(a, document.body);
+// ========================================
+
+// let game = <Game />
+// console.log(game.vdom)
+
+ToyReact.render(<Game />, document.body);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
